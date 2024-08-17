7ea6140c83352137405251d277f2b0d6938e4e28
Javadoff <sabuhi.javadli@solid.az>
2024-08-17 12:58:14 +0400

    fix: mock

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest import mock
 
 import pytest
 
@@ -6,30 +6,30 @@ from src.pytemplate.entrypoints.cli.main import main
 
 
 def test_main_add():
-    with patch("builtins.input", side_effect=["1", "2", "add"]):
+    with mock.patch("builtins.input", side_effect=["1", "2", "add"]):
         result = main()
         assert result == 3
 
 
 def test_main_subtract():
-    with patch("builtins.input", side_effect=["1", "2", "subtract"]):
+    with mock.patch("builtins.input", side_effect=["1", "2", "subtract"]):
         result = main()
         assert result == -1
 
 
 def test_main_multiply():
-    with patch("builtins.input", side_effect=["1", "2", "multiply"]):
+    with mock.patch("builtins.input", side_effect=["1", "2", "multiply"]):
         result = main()
         assert result == 2
 
 
 def test_main_divide():
-    with patch("builtins.input", side_effect=["2", "1", "divide"]):
+    with mock.patch("builtins.input", side_effect=["2", "1", "divide"]):
         result = main()
         assert result == 2
 
 
 def test_main_invalid_action():
-    with patch("builtins.input", side_effect=["1", "2", "invalid"]):
+    with mock.patch("builtins.input", side_effect=["1", "2", "invalid"]):
         with pytest.raises(ValueError):
             main()
