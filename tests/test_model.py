f8ce52837f28758ab9283e8fbae591bdb067d08b
Javadoff <sabuhi.javadli@solid.az>
2024-08-17 11:40:15 +0400

    test: operands_factory

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,16 @@
 import pytest
 
-from src.pytemplate.domain.models import Operands
+from src.pytemplate.domain.models import Operands, operands_factory
 
 
 def test_operands():
     operands = Operands(1, 2)
+    assert operands.first_operand == 1
+    assert operands.second_operand == 2
+
+
+
+def test_operands_factory():
+    operands = operands_factory(1, 2)
     assert operands.first_operand == 1
     assert operands.second_operand == 2
